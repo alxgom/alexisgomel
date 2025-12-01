24e822419d024e379e6b70778388b68fc249ba7e
Alexis <alexis.gomel@gmail.com>
2025-12-01 23:35:20 +0100

    more links in const

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
+export const SITE_TITLE = "Alexis Gomel";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const EMAIL = "alexis.gomel@gmail.com";
+export const INSTAGRAM_LINK = "https://www.instagram.com/lookn_around/";
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/alexis-gomel-95484278/";
+export const GITHUB_LINK = "https://github.com/alxgom";
+export const RESEARCHGATE_LINK = "https://www.researchgate.net/profile/Alexis-Gomel";
+
+
+
