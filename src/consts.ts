dd666b5a23e4c56ee9a9d1f89ba86eddfa0f0e06
Alexis <alexis.gomel@gmail.com>
2026-01-05 19:26:41 +0100

    updated seo schemas

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Alexis Gomel";
-export const SITE_DESCRIPTION = "Personal website of Alexis Gomel, Physicist and Data Scientist. Discover my projects, publications, and photography.";
+export const SITE_DESCRIPTION = "Personal website of Alexis Gomel, Physicist, Data Scientist, Data Analyst, and Researcher based in Barcelona, Spain. Discover my projects, publications, and photography.";
+export const SITE_DESCRIPTION_ES = "Sitio web personal de Alexis Gomel, Físico, Científico de Datos, Analista de Datos e Investigador con base en Barcelona, España. Descubre mis proyectos, publicaciones y fotografía.";
 export const EMAIL = "alexis.gomel@gmail.com";
 export const INSTAGRAM_LINK = "https://www.instagram.com/lookn_around/";
 export const LINKEDIN_LINK = "https://www.linkedin.com/in/alexis-gomel-95484278/";
