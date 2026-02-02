d90e03a28913a7dcc53cd1ef5c3e64443e236cb6
Alexis <alexis.gomel@gmail.com>
2026-02-02 12:51:08 +0100

    updated lokerstudio

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
 	integrations: [
 		mdx(),
 		sitemap({
-			filter: (page) => !page.includes('/font-test'),
+			filter: (page) => !page.includes('/font-test') && !page.includes('/webanalytics'),
 		}),
 		partytown({
 			config: {
