11630dcd45c1312b106bbe46f0e3b2500d6f40a9
Alexis <alexis.gomel@gmail.com>
2026-02-18 18:20:32 +0100

    looker loading screen

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
 	integrations: [
 		mdx(),
 		sitemap({
-			filter: (page) => !page.includes('/font-test') && !page.includes('/webanalytics'),
+			filter: (page) => !page.includes('/font-test'),
 		}),
 	],
 	adapter: cloudflare({
