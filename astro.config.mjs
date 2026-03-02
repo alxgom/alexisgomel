65f83efdd49e45f9e85b72f3d09c3a68a46e3ca6
Alexis <alexis.gomel@gmail.com>
2026-03-02 14:13:38 +0100

    Add Cloudflare adapter (hybrid mode) to fix Wrangler deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://alexisgomel.com",
+	output: "server",
+	adapter: cloudflare(),
 	integrations: [
 		mdx(),
 		sitemap({
